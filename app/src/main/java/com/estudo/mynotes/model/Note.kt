34a2ac18d1a203edd6ef107ebde3effcaa35f764
Wemerson Fernandes Rangel <wemersonf.rangel@outlook.com>
2021-05-26 13:28:11 -0300

    * Habilita ViewBinding na classe NoteDetail * Remove exibição da descrição na tela principal * Exibe detalhes do item na nova tela * Adiciona plugin kotlin-parcelize

--- app/src/main/java/com/estudo/mynotes/model/Note.kt
+++ app/src/main/java/com/estudo/mynotes/model/Note.kt
@@ -1,7 +1,11 @@
 package com.estudo.mynotes.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Note(
     val title: String,
     val description: String,
     val date: String
-)
+): Parcelable
--- app/src/main/java/com/estudo/mynotes/ui/main/NotesAdapter.kt
+++ app/src/main/java/com/estudo/mynotes/ui/main/NotesAdapter.kt
@@ -6,10 +6,10 @@ import androidx.recyclerview.widget.RecyclerView
 import com.estudo.mynotes.databinding.NoteItemBinding
 import com.estudo.mynotes.model.Note
 
-class NotesAdapter(private val notes: MutableList<Note>) :
+class NotesAdapter(private val notes: MutableList<Note>, private val onClickItem: (Note) -> Unit) :
     RecyclerView.Adapter<NotesAdapter.NotesViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NotesViewHolder {
+   override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NotesViewHolder {
         val binding = NoteItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return NotesViewHolder(binding)
     }
@@ -25,8 +25,10 @@ class NotesAdapter(private val notes: MutableList<Note>) :
     inner class NotesViewHolder(private val binding: NoteItemBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(note: Note) {
             binding.noteTitle.text = note.title
-            binding.noteDescription.text = note.description
             binding.noteDate.text = note.date
+            binding.root.setOnClickListener {
+                onClickItem(note)
+            }
         }
     }
 }
